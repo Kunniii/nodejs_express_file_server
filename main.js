092029ef01bae86832ed13f7c85a4d710798b8c1
Kunniii <nguyenairport56@gmail.com>
2023-10-04 04:17:09 +0700

    update UI, added QR, show file size

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ import * as fs from "fs";
 
 const folder = "./files/";
 
+const PORT = 8000;
+
 const app = express();
+
+function formatFileSize(kilobytes) {
+  if (kilobytes >= 1024 * 1024) {
+    // Convert to gigabytes with two decimal places
+    return (kilobytes / (1024 * 1024)).toFixed(2) + " GB";
+  } else if (kilobytes >= 1024) {
+    // Convert to megabytes with two decimal places
+    return (kilobytes / 1024).toFixed(2) + " MB";
+  } else {
+    // Display in kilobytes
+    return kilobytes + " KB";
+  }
+}
 
 app.set("view engine", "pug");
 app.use(express.static("public"));
@@ -11,7 +26,11 @@ app.get("/", (req, res) => {
   let files = fs.readdirSync(folder);
   let items = [];
   for (let path of files) {
-    items.push({ url: path });
+    let stats = fs.statSync(`${folder}/${path}`);
+    items.push({
+      url: path,
+      size: formatFileSize(Math.round(stats.size / 1024)),
+    });
   }
   if (files.length > 0) {
     res.render("index", {
@@ -36,9 +55,9 @@ app.get("/get/:fileName", (req, res) => {
   }
 });
 
-app.listen(8001, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   }
-  console.log("listen on port 8001");
+  console.log(`listening on port ${PORT}`);
 });
--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
 // scripts
+// Get a reference to the HTML element
+
+function generateQR(file_url, id) {
+  let full_url = `http://${location.hostname}:${location.port}/${file_url}`;
+  let qrcodeContainer = document.getElementById(`qr-code-${id}`);
+  if (!qrcodeContainer.innerHTML) {
+    new QRCode(qrcodeContainer, {
+      text: full_url,
+      width: 200,
+      height: 200,
+    });
+  } else {
+    qrcodeContainer.innerHTML = null;
+  }
+}
